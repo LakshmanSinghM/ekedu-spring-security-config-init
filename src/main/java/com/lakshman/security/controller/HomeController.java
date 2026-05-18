578d1625e471672c1d73da5525b1c091494a04ec
Lakshman Singh <lslakshman2024@gmail.com>
2026-05-18 22:06:39 +0530

    Done with the spring init configurations

--- src/main/java/com/lakshman/security/controller/HomeController.java
+++ src/main/java/com/lakshman/security/controller/HomeController.java
@@ -3,15 +3,16 @@ package com.lakshman.security.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 @RestController
 public class HomeController {
 
+    // secured needs login
     @GetMapping("/health")
     public String healthCheck() {
         return "Health check done application running fine";
     }
 
+    // public no need of the login
     @GetMapping
     public String home() {
         return "Application running fine";
